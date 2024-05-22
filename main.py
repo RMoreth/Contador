264e1af0c12164a2368a1b7f5c476792a9d77dee
Rmoreth <ricardoarcemoreth@gmail.com>
2024-05-21 21:05:21 -0300

    implementação de tempo

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from time import sleep
-
+import os
+# Usuario informa um numero
 contador = int(input("informe um número inteiro: "))
+os.system('cls')
+
+# conta a partir do numero informado ate 0
 while contador >= 0:
-    print(contador)
+    print(f"contagem regressiva: {contador}...")
     contador -= 1
-    sleep(0.2)
-print('fim')
+    sleep(1)
+    os.system('cls')
+print('BOOM')
